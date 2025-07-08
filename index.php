e10427ec2af3a50270973e63e5518678f96b9aff
Andrew Kasyanov <kasyianovan@reach-stacker.ru>
2025-07-08 17:40:01 +0300

    080725 work

--- index.php
+++ index.php
@@ -24,7 +24,32 @@ require_once __DIR__ . '/src/helpers.php';
     <link rel="stylesheet" href="/css/style.css?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/css/style.css'); ?>" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <script src="https://www.google.com/recaptcha/api.js?render=6LdQcW4rAAAAAIgzlEnFq_v6nTtU44mVIa2IfZ1n"></script>   
+    <script src="https://www.google.com/recaptcha/api.js?render=6LdQcW4rAAAAAIgzlEnFq_v6nTtU44mVIa2IfZ1n"></script>  
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();
+       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(103253679, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/103253679" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter --> 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FVJVWRVKC1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-FVJVWRVKC1');
+    </script>
 </head>
 <body>
     <!-- Шапка -->
